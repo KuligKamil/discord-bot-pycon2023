e6510334ffea170d95c15f60576d7ca29f6e2fd7
KuligKamil <kamil.marek.kulig@gmail.com>
2023-06-27 01:36:53 +0200

    💪 chore: improve only necessary code

--- src/cogs/gamer.py
+++ src/cogs/gamer.py
@@ -3,14 +3,8 @@ from discord.ext import commands
 
 
 class GammerCogs(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        channel = member.guild.system_channel
-        if channel is not None:
-            await channel.send(f'Welcome {member.mention}.')
 
 
     @discord.app_commands.command(name="hello", description="Bot says hello to you")
