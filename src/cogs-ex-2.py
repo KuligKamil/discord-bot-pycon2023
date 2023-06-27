c186904d718bcd3f3035fd7f9ed22fc19bede391
sinem-ay <sinem.ayyildiz98@gmail.com>
2023-06-27 12:25:53 +0200

    chore: add env.py file

--- src/cogs-ex-2.py
+++ src/cogs-ex-2.py
@@ -1,4 +1,4 @@
-import os
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
+from env import DISCORD_TOKEN, CHANNEL_ID, GUILD_ID
--- src/cogs/message.py
+++ src/cogs/message.py
@@ -1,4 +1,4 @@
-import os
 import discord
 from discord.ext import commands
 from discord import app_commands
+from env import GUILD_ID
